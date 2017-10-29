5e996ba77f41bab20a9f6738386cd6210c4c7225
Kexone <kami.extreme@gmail.com>
2017-10-29 18:08:14 +0100

    little changes

--- ANO2_parking/main.cpp
+++ ANO2_parking/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {	
 	Classifier classifier("strecha1_map.txt", "train_vector.txt", "svm.xml");
-	classifier.train("full.txt", "free.txt");
+	//classifier.train("full.txt", "free.txt");
 	classifier.recognize("test.txt", "results.txt");
 	classifier.evaluate("groundtruth.txt", "results.txt");
     return 0;
